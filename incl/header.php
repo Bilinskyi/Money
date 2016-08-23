8936b3372991e2284ebd8480e209f19ed56774b7
Taras Bilinskyi <1unitedcrew@gmail.com>
2016-08-23 11:27:23 +0300

    v10

--- incl/header.php
+++ incl/header.php
@@ -3,7 +3,6 @@
   <link rel="stylesheet" href="css/style.min.css">
   <link rel="stylesheet" href="fonts/fonts.css">  
   <link rel="stylesheet" href="bx-slider/jquery.bxslider.css">
-  <link rel="stylesheet" href="http://code.jquery.com/ui/1.12.0/themes/base/jquery-ui.css">
   <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js">
   </script><script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
